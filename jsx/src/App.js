3f7bc82d2b069718159a33551fe8d97746ac4e56
Muzammil Ayaz <muzammilayaz.kabeer@e-consystems.com>
2023-02-03 00:37:03 +0530

    creating manual react element

---
+++ jsx/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import CreateTest from './Components/testComp';
+
+function App() {
+  // console.log(CreateTest)
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default App;
